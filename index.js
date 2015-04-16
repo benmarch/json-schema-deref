57f1302f383e306c46f9bebcfce13555a21d41e2
benmarch <bmarch89@gmail.com>
2015-04-15 22:05:18 -0400

    es5 commit

---
+++ index.js
@@ -0,0 +1,38 @@
+(function () {
+    function getSchemaFromRefString(schema, refString) {
+        var pieces = refString.split('/');
+        if (pieces.shift() !== '#') {
+            throw new Error('Local references only!');
+        }
+
+        return deref(pieces.reduce(function (prevObj, key) {
+            if (!prevObj[key]) {
+                throw new Error('Local references only!');
+            }
+            return prevObj[key];
+        }, schema));
+    }
+
+    function deref(schema) {
+        if (!schema.properties) {
+            return schema;
+        }
+
+        Object.keys(schema.properties).forEach(function (propertyName) {
+            if (schema.properties.hasOwnProperty(propertyName)) {
+                var property = schema.properties[propertyName];
+                Object.keys(property).forEach(function (key) {
+                    if (property.hasOwnProperty(key)) {
+                        if (key === '$ref') {
+                            property[key] = getSchemaFromRefString(schema, property[key]);
+                        }
+                    }
+                });
+            }
+        });
+
+        return schema;
+    }
+
+    return deref;
+}());
